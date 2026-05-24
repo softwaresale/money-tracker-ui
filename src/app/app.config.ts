4acd0e65ffcc72553f62e90e3707b3dd7e5df875
Charlie Sale <softwaresale01@gmail.com>
2026-05-24 13:51:43 -0400

    Add auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,32 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { UserManagerSettings } from 'oidc-client-ts';
+import { initOidc, OIDC_ROUTES, authzTokenInterceptor } from '@edgeflare/ngx-oidc';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+
+const oidcConfig: UserManagerSettings = {
+  authority: 'https://salefamily-hdmlgu.us1.zitadel.cloud/',
+  client_id: '374377270212596762',
+  redirect_uri: "http://localhost:4200/signin/callback",
+  response_type: "code",
+  scope: "openid profile email",
+  post_logout_redirect_uri: 'http://localhost:4200',
+  automaticSilentRenew: true,
+  loadUserInfo: true,
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+
+    // oidc config
+    initOidc(oidcConfig),
+    provideRouter(OIDC_ROUTES),
+
     provideRouter(routes, withComponentInputBinding()),
+
+    provideHttpClient(withFetch(), withInterceptors([authzTokenInterceptor])),
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { HouseholdExpensesPage } from './household-expenses-page/household-expenses-page';
 import { AddExpensePage } from './household-expenses-page/add-expense-page/add-expense-page';
+import { authGuard } from '@edgeflare/ngx-oidc';
 
 export const routes: Routes = [
     {
         path: 'households',
+        canActivate: [authGuard],
+        canActivateChild: [authGuard],
         children: [
             {
                 path: ':id',
